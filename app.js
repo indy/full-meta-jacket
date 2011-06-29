897353715aac660ba585f2bb86d8cd2200680fce
Inderjit Gill <email@indy.io>
2011-06-29 13:15:05 +0100

    passing in nested data structures to templates

--- app.js
+++ app.js
@@ -30,9 +30,18 @@ app.configure('production', function(){
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Eugenia',
+    nested: {name: 'nested name'}
   });
 });
+
+app.get('/blog', function(req, res){
+  res.render('index', {
+    title: 'blog',
+    nested: {name: 'nested name from blog'}
+  });
+});
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
