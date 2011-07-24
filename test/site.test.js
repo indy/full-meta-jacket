aa404dcefa419724c066d1be4fc41a12eb01a90e
Inderjit Gill <email@indy.io>
2011-07-24 18:50:06 +0100

    added unit tests for buildMetadata

--- test/site.test.js
+++ test/site.test.js
@@ -4,8 +4,25 @@ var fs = require('fs');
 
 var prefix = 'test/files/';
 
-exports['traverse filesystem'] = function() {
-  var res = site.traverse('test/files/traverse-1');
+exports['build metadata'] = function() {
+  var m = site.buildMetadata('test/files/traverse-1');
 
-  assert.eql(res, 42);
+  // number of files in a folder
+  assert.eql(3, m._files.length);
+  assert.eql(1, m.foo._files.length);
+  // implicit file metadata
+  assert.eql("a1", m.a1.filename);
+  assert.eql("b2", m.b2.filename);
+  assert.eql("c3", m.c3.filename);
+  // implicit file metadata (in a sub-directory)
+  assert.eql("d4", m.foo.d4.filename);
+
+  // top-level zonal metadata
+  assert.eql("Catch-22", m['book-title']);
+  // zonal metadata in a sub-directory
+  assert.eql("A journal", m.journal['journal-title']);
+
+  // sub-directory posts
+
+//  assert.eql(m, 42);
 }
