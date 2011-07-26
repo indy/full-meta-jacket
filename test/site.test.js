f115ec4bcd6016ec3e4b65f8240ad323d86b4a2a
Inderjit Gill <email@indy.io>
2011-07-26 22:49:37 +0100

    added eval file tests

--- test/site.test.js
+++ test/site.test.js
@@ -26,8 +26,33 @@ exports['build metadata'] = function() {
 
 }
 
-exports['render folder'] = function() {
-//  var meta = site.buildMetadata('test/files/traverse-2');
-//  site.renderFolder(meta, 'test/files/traverse-2', 'tmp/test-traverse-2');
-  assert.eql(2, 2);
+exports['eval file'] = function() {
+
+  var meta = site.buildMetadata('test/files/eval-file');
+  var res = site.evalFile(meta, 'test/files/eval-file/a1');
+//  console.log(meta);
+//  console.log(res);
+  var expected = fs.readFileSync('test/files/eval-file-expected/a1', 'utf8');
+  assert.equal(res, expected);
 }
+
+/*
+exports['layout file'] = function() {
+
+  var meta = site.buildMetadata('test/files/layout-file');
+  var res = site.evalFile(meta, 'test/files/layout-file/a1');
+  // var layout = site.layoutForFile('test/files/layout-file/a1');
+  site.layout(layout, res);
+  var expected = fs.readFileSync('test/files/layout-file-expected/a1', 'utf8');
+//  assert.equal(res, expected);
+}
+  */
+
+
+
+/*
+  3 stages are:
+    build metadata
+    eval file using metadata
+    layout file
+ */
