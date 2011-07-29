aaa593960c27aefeb54ed67ff9f81617b636bae0
Inderjit Gill <email@indy.io>
2011-07-29 21:35:51 +0100

    split metadata tests into multiple unit tests

--- test/metadata.test.js
+++ test/metadata.test.js
@@ -1,27 +1,36 @@
 var assert = require('assert');
 var metadata = require('../lib/metadata');
 
-exports['build metadata'] = function() {
-  var m = metadata.build('test/files/traverse-1');
 
-  // number of files in a folder
+// load metadata for one heirarchy and test multiple aspects from it
+var m = metadata.build('test/files/traverse-1');
+
+
+exports['number of files in a folder'] = function() {
   assert.eql(3, m._files.length);
   assert.eql(1, m.foo._files.length);
+}
 
-  // number of subdirectories, ignoring the 'layouts' directory
+exports['number of subdirectories, ignoring layouts directory'] = function() {
   assert.eql(2, m._directories.length);
+}
 
-  // implicit file metadata
+exports['implicit file metadata'] = function() {
   assert.eql("a1", m.a1.filename);
   assert.eql("b2", m.b2.filename);
   assert.eql("c3", m.c3.filename);
-  // implicit file metadata (in a sub-directory)
-  assert.eql("d4", m.foo.d4.filename);
+}
 
-  // top-level zonal metadata
+exports['implicit file metadata (in a sub-directory)'] = function() {
+  assert.eql("d4", m.foo.d4.filename);
+}
+
+exports['top-level zonal metadata'] = function() {
   assert.eql("Catch-22", m['book-title']);
-  // zonal metadata in a sub-directory
+}
+
+exports['zonal metadata in a sub-directory'] = function() {
   assert.eql("A journal", m.journal['journal-title']);
+}
 
   // sub-directory posts
-}
