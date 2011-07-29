8b6c825463d7ce028a2b3958093a1997e74ee0f3
Inderjit Gill <email@indy.io>
2011-07-29 21:49:51 +0100

    layout test works from a single test variable

--- test/layout.test.js
+++ test/layout.test.js
@@ -3,8 +3,9 @@ var layout = require('../lib/layout');
 var utils = require('../lib/utils');
 var fs = require('fs');
 
-exports['build layout hash'] = function() {
-  var layouts = layout.build('test/files/layout');
+// load layouts once, test multiple aspects
+var layouts = layout.build('test/files/layout');
 
+exports['read all layouts'] = function() {
   assert.equal(utils.objectSize(layouts), 3);
 }
