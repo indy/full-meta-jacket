73a85d6003bf3e50091c4557f50ae76093baa56d
Inderjit Gill <email@indy.io>
2011-08-10 21:26:46 +0100

    modified app.js just to get it working at a basic level

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.configure('production', function(){
 });
 
 // Routes
-
+/*
 app.get('/', function(req, res){
   res.render('index', {
     title: 'murmur',
@@ -42,7 +42,7 @@ app.get('/blog', function(req, res){
     nested: {name: 'nested name from blog'}
   });
 });
-
+*/
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
