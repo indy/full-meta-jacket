4fc97a4fa4739135917cfc85d6bf292a2d6f6b8e
Inderjit Gill <email@indy.io>
2011-08-16 23:21:15 +0100

    finished published_date test

--- test/published_date.test.js
+++ test/published_date.test.js
@@ -1,17 +1,22 @@
 var assert = require('assert');
 var site = require('../lib/site');
+var metadata = require('../lib/metadata');
 var fs = require('fs');
 
 
 exports['published date'] = function() {
 
-  // render the published_date test folder
-  site.build('test/files/published_date', 'test/tmp');
+  // 3 of the 4 posts in test/files/published_date have a 'published_date'
+  // attribute. This tests to make sure they're copied into a published
+  // object and ordered correctly
 
-  // compare the results
+  var meta = metadata.build('test/files/published_date');
+  assert.equal(meta.journal._posts._published.length, 3);
 
-  // load layouts once, test multiple aspects
-//  var layouts = layout.build('test/files/layout');
-//  assert.equal(utils.objectSize(layouts), 3);
+  var expectedDates = ["2011-08-16", "2011-09-16", "2011-10-16"];
+  var published = meta.journal._posts._published;
+  for(var i=0;i<expectedDates.length;i++){
+    assert.equal(published[i].page.published_date, expectedDates[i]);
+  }
 
 }
