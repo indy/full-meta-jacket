75f51ce1c7d6d9c623d919623d847c93996a26ee
Inderjit Gill <email@indy.io>
2011-08-21 15:31:20 +0100

    new metadata passing initial structure tests

--- test/metadata2.test.js
+++ test/metadata2.test.js
@@ -1,42 +1,24 @@
 var assert = require('assert');
 var metadata = require('../lib/metadata');
+var utils = require('../lib/utils');
 
 
 // load metadata for one heirarchy and test multiple aspects from it
-var m = metadata.build('test/files/traverse-1');
+var m = metadata.build('test/files/traverse-2');
+utils.writeMeta('../new-meta.js', m);
 
-//console.log(m);
 
+exports['metadata structured according to how content will be served'] = function() {
+  assert.ok(m['index.html']);
 
-exports['number of files in a folder'] = function() {
-  assert.eql(3, m._files.length);
-  assert.eql(1, m.foo._files.length);
+  // also check that posts are in the right place
+  assert.ok(m['journal']['a1.html']);
+  assert.ok(m['journal']['b2.html']);
+  assert.ok(m['journal']['c3.html']);
 }
 
-exports['number of subdirectories, ignoring layouts directory'] = function() {
-  assert.eql(2, m._directories.length);
-}
 
-exports['implicit file metadata'] = function() {
-  assert.eql("a1", m.a1.filename);
-  assert.eql("b2", m.b2.filename);
-  assert.eql("c3", m.c3.filename);
-}
+//exports['local variables in file'] = function() {
+//  assert.eql("murmur@example.com", m['index.html'].locals.email);
+//}
 
-exports['explicit file metadata'] = function() {
-  assert.eql(3, m.a1.names.length);
-}
-
-exports['implicit file metadata (in a sub-directory)'] = function() {
-  assert.eql("d4", m.foo.d4.filename);
-}
-
-exports['top-level zonal metadata'] = function() {
-  assert.eql("Catch-22", m['book-title']);
-}
-
-exports['zonal metadata in a sub-directory'] = function() {
-  assert.eql("A journal", m.journal['journal-title']);
-}
-
-  // sub-directory posts
