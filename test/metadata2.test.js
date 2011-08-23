e22626c3c022f39a679ae9630ff6c528c2e09687
Inderjit Gill <email@indy.io>
2011-08-23 22:20:29 +0100

    added metadata.scopedBuild

--- test/metadata2.test.js
+++ test/metadata2.test.js
@@ -7,6 +7,41 @@ var utils = require('../lib/utils');
 var m = metadata.fullBuild('test/files/traverse-2');
 utils.writeMeta('../new-meta.js', m);
 
+exports['scopedBuild index.html'] = function() {
+  var scoped = metadata.scopedBuild(m, '/index.html');
+  assert.eql(scoped.email, "murmur@example.com");
+  assert.eql(scoped.filename, "index.imd");
+  assert.eql(scoped.siteName, "FakeSite");
+}
+
+exports['scopedBuild journal/a1.html'] = function() {
+  var scoped = metadata.scopedBuild(m, '/journal/a1.html');
+
+  assert.eql(scoped.posts.length, 3);
+  assert.eql(scoped['journal-title'], "A journal");
+
+  assert.eql(scoped["published_date"], "2011-08-16");
+  assert.eql(scoped["filename"], "a1.imd");
+  assert.eql(scoped["publishedFilename"], "a1.html");
+  assert.eql(scoped.names.length, 3);
+
+  assert.eql(scoped.siteName, "FakeSite");
+}
+
+exports['scopedBuild journal/c3.html'] = function() {
+  var scoped = metadata.scopedBuild(m, '/journal/c3.html');
+
+  assert.eql(scoped.posts.length, 3);
+  // shadowing the journal scope variable with a c3 specific one
+  assert.eql(scoped['journal-title'], "custom title");
+
+  assert.eql(scoped["published_date"], "2011-10-16");
+  assert.eql(scoped["filename"], "c3.imd");
+  assert.eql(scoped["publishedFilename"], "c3.html");
+  assert.eql(scoped.names.length, 3);
+
+  assert.eql(scoped.siteName, "FakeSite");
+}
 
 exports['metadata structured around served content'] = function() {
   assert.ok(m['index.html']);
