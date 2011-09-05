8fd72545bbaef57fc064bfcf494869cd03d26697
Inderjit Gill <email@indy.io>
2011-09-05 23:01:31 +0100

    updated filters

--- test/sitetemp.js
+++ test/sitetemp.js
@@ -3,9 +3,6 @@ var site = require('../lib/site');
 
 
 exports['build site'] = function() {
-//  site.build('/Users/indy/mem/murmur/test/files/metadata', '/Users/indy/mem/ou');
-//  site.build('/Users/indy/mem/bar', '/Users/indy/mem/outbar');
-  site.build('/Users/indy/work/indy.io', '/Users/indy/mem/murmur/public');
-//  site.build('/Users/indy/work/murmur-temp/source', '/Users/indy/mem/dest');
-//  site.build('/Users/indy/mem/source2', '/Users/indy/mem/dest2');
+  site.build('/Users/indy/mem/indy.io', '/Users/indy/mem/murmur/public');
+//  site.build('/Users/indy/work/indy.io', '/Users/indy/mem/murmur/public');
 }
