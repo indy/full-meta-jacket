7512c2690bdd431130a467e1693d1bbc498a4d07
Inderjit Gill <email@indy.io>
2011-09-17 14:20:17 +0100

    improved way that metadata titles and dates are derived

--- test/metadata2.test.js
+++ test/metadata2.test.js
@@ -7,46 +7,46 @@ var m = metadata.fullBuild('test/files/metadata');
 
 exports['scopedBuild index.html'] = function() {
   var scoped = metadata.scopedBuild(m, '/index.html');
-  assert.eql(scoped.email, "murmur@example.com");
-  assert.eql(scoped._filename, "/index.imd");
-  assert.eql(scoped.siteName, "FakeSite");
+  assert.equal(scoped.email, "murmur@example.com");
+  assert.equal(scoped._filename, "/index.imd");
+  assert.equal(scoped.siteName, "FakeSite");
 }
 
 exports['scopedBuild journal/a1.html'] = function() {
   var scoped = metadata.scopedBuild(m, '/journal/a1.html');
 
-  assert.eql(scoped.posts.length, 3);
-  assert.eql(scoped['journal-title'], "A journal");
+  assert.equal(scoped.posts.length, 3);
+  assert.equal(scoped['journal-title'], "A journal");
 
-  assert.eql(scoped["published_date"], "2011-08-16");
-  assert.eql(scoped._filename, "/journal/_posts/a1.imd");
-  assert.eql(scoped["publishedFilename"], "a1.html");
-  assert.eql(scoped.names.length, 3);
+  assert.eql(scoped["date"], new Date(2011, 07, 16));
+  assert.equal(scoped._filename, "/journal/_posts/a1.imd");
+  assert.equal(scoped["publishedFilename"], "a1.html");
+  assert.equal(scoped.names.length, 3);
 
-  assert.eql(scoped.siteName, "FakeSite");
+  assert.equal(scoped.siteName, "FakeSite");
 }
 
 exports['scopedBuild journal/c3.html'] = function() {
   var scoped = metadata.scopedBuild(m, '/journal/c3.html');
 
-  assert.eql(scoped.posts.length, 3);
+  assert.equal(scoped.posts.length, 3);
   // shadowing the journal scope variable with a c3 specific one
-  assert.eql(scoped['journal-title'], "custom title");
+  assert.equal(scoped['journal-title'], "custom title");
 
-  assert.eql(scoped["published_date"], "2011-10-16");
-  assert.eql(scoped._filename, "/journal/_posts/c3.imd");
-  assert.eql(scoped["publishedFilename"], "c3.html");
-  assert.eql(scoped.names.length, 3);
+  assert.eql(scoped["date"], new Date(2011, 9, 16));
+  assert.equal(scoped._filename, "/journal/_posts/c3.imd");
+  assert.equal(scoped["publishedFilename"], "c3.html");
+  assert.equal(scoped.names.length, 3);
 
-  assert.eql(scoped.siteName, "FakeSite");
+  assert.equal(scoped.siteName, "FakeSite");
 }
 
 exports['scopedBuild for simple content'] = function() {
   var scoped = metadata.scopedBuild(m, '/simple/simple.html');
-  assert.eql(scoped._filename, '/simple/simple.html');
+  assert.equal(scoped._filename, '/simple/simple.html');
 
   scoped = metadata.scopedBuild(m, '/simple/simple.png');
-  assert.eql(scoped._directCopy, true);
+  assert.equal(scoped._directCopy, true);
 }
 
 exports['metadata structured around served content'] = function() {
@@ -61,13 +61,10 @@ exports['metadata structured around served content'] = function() {
 exports['posts metadata'] = function() {
   var postsMeta = m['journal']['_locals']['posts'];
 
-  assert.eql(postsMeta.length, 3);
+  assert.equal(postsMeta.length, 3);
 }
 
-// note: filters that iterate through posts will need to check published_date
-
-
 exports['local variables in file'] = function() {
-  assert.eql("murmur@example.com", m['index.html']._locals.email);
+  assert.equal("murmur@example.com", m['index.html']._locals.email);
 }
 
